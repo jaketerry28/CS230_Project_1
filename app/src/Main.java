d12104695d460833440cdd8e36c473d972f26d87
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-10-17 14:58:30 -0400

    added comments

--- app/src/Main.java
+++ app/src/Main.java
@@ -27,9 +27,9 @@ public class Main {
 //        System.out.println(data.instructions.get(1).get(0));
 //
 //        for (int i : data.instructions.keySet()) {
-//            if (data.instructions.get(i).get(0).contains("BR")) {
-//                String labelAddress = data.instructions.get(i).get(1);
-//                System.out.println(data.branches.get(labelAddress));
+//            if (data.instructions.get(i).get(0).contains("BR")) { // if first list element of each key has 'BR'
+//                String labelAddress = data.instructions.get(i).get(1); // get the branching label 
+//                System.out.println(data.branches.get(labelAddress)); // use label as key to access byte address inside branches hashMap
 //            }
 //        } // end for
     }
