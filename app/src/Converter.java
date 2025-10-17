5b7838462fc772c31a874050aa125eac9acb4f5b
eldon <eldonjohnson04@gmail.com>
2025-10-17 16:42:00 -0400

    More complete converter, instructions class to simplify things

--- app/src/Converter.java
+++ app/src/Converter.java
@@ -1,22 +1,63 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Converter {
-    public static void toHexa() {
-        Partitioner partitioner = new Partitioner();
-        HashMap<Integer, List<String>> instructions = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        if (instructions.containsValue("LDBA")) {
-            list.add("D");
-        } else if (instructions.containsValue("STBA")) {
-            list.add("F");
-        } else if (instructions.containsValue("STWA")) {
-            list.add("E");
-        } else if (instructions.containsValue("ANDA")) {
-            list.add("8");
-        } else if (instructions.containsValue("STWA")) {
-            list.add("E1");
+    HashMap<String, String> convertValues = new HashMap<>();
+    Instructions instructions = new Instructions();
+
+    public HashMap<String, String> instructionConvert(String[] args) {
+        Partitioner.ParsedData data = Partitioner.parse(args[0]);
+
+        //looks to see what instruction set is present and gets the corresponding value
+        for (int i : data.instructions.keySet()) {
+            if (data.instructions.get(i).get(1).contains("LDBA")) {
+                convertValues.put("Output1", instructions.getLDBA());
+            }
+            else if (data.instructions.get(i).get(1).contains("LDWA")) {
+                convertValues.put("Output1", instructions.getLDWA());
+            }
+            else if (data.instructions.get(i).get(1).contains("STBA")) {
+                convertValues.put("Output1", instructions.getSTBA());
+            }
+            else if (data.instructions.get(i).get(1).contains("STWA")) {
+                convertValues.put("Output1", instructions.getSTWA());
+            }
+            else if (data.instructions.get(i).get(1).contains("ADDA")) {
+                convertValues.put("Output1", instructions.getADDA());
+            }
+            else if (data.instructions.get(i).get(1).contains("ASLA")) {
+                convertValues.put("Output1", instructions.getASLA());
+            }
+            else if (data.instructions.get(i).get(1).contains("ASRA")) {
+                convertValues.put("Output1", instructions.getASRA());
+            }
+            else if (data.instructions.get(i).get(1).contains("STOP")) {
+                convertValues.put("Output1", instructions.getSTOP());
+            }
+            else if (data.instructions.get(i).get(1).contains("CPBA")) {
+                convertValues.put("Output1", instructions.getCPBA());
+            }
+            else if (data.instructions.get(i).get(1).contains("BRNE")) {
+                convertValues.put("Output1", instructions.getBRNE());
+            }
         }
+        return convertValues;
+    }
+    public HashMap<String, String> hexaConvert(String[] args) {
+        Partitioner.ParsedData data = Partitioner.parse(args[0]);
+
+        return convertValues;
+    }
+    public HashMap<String, String> addressingTypeConvert(String[] args) {
+        Partitioner.ParsedData data = Partitioner.parse(args[0]);
+
+        for (int i : data.instructions.keySet()) {
+            if (data.instructions.get(i).get(3).contains("i")) {
+                convertValues.put("Output2", "0");
+            }
+            else if (data.instructions.get(i).get(3).contains("d")) {
+                convertValues.put("Output2", "1");
+            }
+        }
+        return convertValues;
     }
 }
---
+++ app/src/Instructions.java
@@ -0,0 +1,54 @@
+public class Instructions {
+
+    //used to call upon to make conversion easier
+    public String getLDBA() {
+        String LDBA;
+        LDBA = "D";
+        return LDBA;
+    }
+    public String getLDWA() {
+        String LDWA;
+        LDWA = "C";
+        return LDWA;
+    }
+    public String getSTBA() {
+        String STBA;
+        STBA = "F";
+        return STBA;
+    }
+    public String getSTWA() {
+        String STWA;
+        STWA = "E";
+        return STWA;
+    }
+    public String getADDA() {
+        String ADDA;
+        ADDA = "6";
+        return ADDA;
+    }
+    public String getASLA() {
+        String ASLA;
+        ASLA = "0A";
+        return ASLA;
+    }
+    public String getASRA() {
+        String ASRA;
+        ASRA = "0C";
+        return ASRA;
+    }
+    public String getSTOP() {
+        String STOP;
+        STOP = "00";
+        return STOP;
+    }
+    public String getCPBA() {
+        String CPBA;
+        CPBA = "B";
+        return CPBA;
+    }
+    public String getBRNE() {
+        String BRNE;
+        BRNE = "1A";
+        return BRNE;
+    }
+}
