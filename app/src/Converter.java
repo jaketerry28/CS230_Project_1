49e97694b5c65172662ab7a1d39f8ccb2c2d5b04
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-10-17 21:14:51 -0400

    conversion of instrucitons

--- app/src/Converter.java
+++ app/src/Converter.java
@@ -1,63 +1,63 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Converter {
-    HashMap<String, String> convertValues = new HashMap<>();
+    ArrayList instruction_list = new ArrayList();
     Instructions instructions = new Instructions();
 
-    public HashMap<String, String> instructionConvert(String[] args) {
-        Partitioner.ParsedData data = Partitioner.parse(args[0]);
+    public ArrayList instructionConvert(Partitioner.ParsedData data) {
 
         //looks to see what instruction set is present and gets the corresponding value
         for (int i : data.instructions.keySet()) {
-            if (data.instructions.get(i).get(1).contains("LDBA")) {
-                convertValues.put("Output1", instructions.getLDBA());
+            if (data.instructions.get(i).get(0).contains("LDBA")) {
+                convertValues.add(instructions.getLDBA());
             }
-            else if (data.instructions.get(i).get(1).contains("LDWA")) {
-                convertValues.put("Output1", instructions.getLDWA());
+            else if (data.instructions.get(i).get(0).contains("LDWA")) {
+                convertValues.add(instructions.getLDWA());
             }
-            else if (data.instructions.get(i).get(1).contains("STBA")) {
-                convertValues.put("Output1", instructions.getSTBA());
+            else if (data.instructions.get(i).get(0).contains("STBA")) {
+                convertValues.add(instructions.getSTBA());
             }
-            else if (data.instructions.get(i).get(1).contains("STWA")) {
-                convertValues.put("Output1", instructions.getSTWA());
+            else if (data.instructions.get(i).get(0).contains("STWA")) {
+                convertValues.add(instructions.getSTWA());
             }
-            else if (data.instructions.get(i).get(1).contains("ADDA")) {
-                convertValues.put("Output1", instructions.getADDA());
+            else if (data.instructions.get(i).get(0).contains("ADDA")) {
+                convertValues.add(instructions.getADDA());
             }
-            else if (data.instructions.get(i).get(1).contains("ASLA")) {
-                convertValues.put("Output1", instructions.getASLA());
+            else if (data.instructions.get(i).get(0).contains("ASLA")) {
+                convertValues.add(instructions.getASLA());
             }
-            else if (data.instructions.get(i).get(1).contains("ASRA")) {
-                convertValues.put("Output1", instructions.getASRA());
+            else if (data.instructions.get(i).get(0).contains("ASRA")) {
+                convertValues.add(instructions.getASRA());
             }
-            else if (data.instructions.get(i).get(1).contains("STOP")) {
-                convertValues.put("Output1", instructions.getSTOP());
+            else if (data.instructions.get(i).get(0).contains("STOP")) {
+                convertValues.add(instructions.getSTOP());
             }
-            else if (data.instructions.get(i).get(1).contains("CPBA")) {
-                convertValues.put("Output1", instructions.getCPBA());
+            else if (data.instructions.get(i).get(0).contains("CPBA")) {
+                convertValues.add(instructions.getCPBA());
             }
-            else if (data.instructions.get(i).get(1).contains("BRNE")) {
-                convertValues.put("Output1", instructions.getBRNE());
-            }
-        }
-        return convertValues;
-    }
-    public HashMap<String, String> hexaConvert(String[] args) {
-        Partitioner.ParsedData data = Partitioner.parse(args[0]);
-
-        return convertValues;
-    }
-    public HashMap<String, String> addressingTypeConvert(String[] args) {
-        Partitioner.ParsedData data = Partitioner.parse(args[0]);
-
-        for (int i : data.instructions.keySet()) {
-            if (data.instructions.get(i).get(3).contains("i")) {
-                convertValues.put("Output2", "0");
-            }
-            else if (data.instructions.get(i).get(3).contains("d")) {
-                convertValues.put("Output2", "1");
+            else if (data.instructions.get(i).get(0).contains("BRNE")) {
+                convertValues.add(instructions.getBRNE());
             }
         }
         return convertValues;
+    // }
+    // public HashMap<String, String> hexaConvert(String[] args) {
+    //     Partitioner.ParsedData data = Partitioner.parse(args[0]);
+
+    //     return convertValues;
+    // }
+    // public HashMap<String, String> addressingTypeConvert(String[] args) {
+    //     Partitioner.ParsedData data = Partitioner.parse(args[0]);
+
+    //     for (int i : data.instructions.keySet()) {
+    //         if (data.instructions.get(i).get(3).contains("i")) {
+    //             convertValues.put("Output2", "0");
+    //         }
+    //         else if (data.instructions.get(i).get(3).contains("d")) {
+    //             convertValues.put("Output2", "1");
+    //         }
+    //     }
+    //     return convertValues;
     }
 }
+
--- app/src/Instructions.java
+++ app/src/Instructions.java
@@ -1,4 +1,68 @@
+import java.util.*;
+
 public class Instructions {
+
+    ArrayList convertValues = new ArrayList();
+    String addressingMode;
+    String instructionMode;
+    String bitInstructions;
+    String value;
+
+    public ArrayList instructionConvert(Partitioner.ParsedData data) {
+        //looks to see what instruction set is present and gets the corresponding value
+        for (int i : data.assembly_code.keySet()) {
+
+            // get addressing type
+            if (data.assembly_code.get(i).size() > 2){
+                addressingMode = getAddressingMode(data.assembly_code.get(i).get(2));
+            }
+            else{
+                addressingMode = "";
+            }
+
+            if (data.assembly_code.get(i).get(0).contains("LDBA")) {
+                instructionMode = this.getLDBA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("LDWA")) {
+                instructionMode = this.getLDWA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("STBA")) {
+                instructionMode = this.getSTBA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("STWA")) {
+                instructionMode = this.getSTWA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("ADDA")) {
+                instructionMode = this.getADDA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("ASLA")) {
+                instructionMode = this.getASLA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("ASRA")) {
+                instructionMode = this.getASRA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("STOP")) {
+                instructionMode = this.getSTOP();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("CPBA")) {
+                instructionMode = this.getCPBA();
+            }
+            else if (data.assembly_code.get(i).get(0).contains("BRNE")) {
+                instructionMode = this.getBRNE();
+                addressingMode = "";
+            }
+
+            // strips 0x or gets the bit position of a label
+            value = data.assembly_code.get(i).get(1);
+            value = getValue(data, value);
+
+            bitInstructions = instructionMode + addressingMode;
+            convertValues.add(bitInstructions);
+            convertValues.add(value);
+
+        }
+        return convertValues;
+    }
 
     //used to call upon to make conversion easier
     public String getLDBA() {
@@ -51,4 +115,28 @@ public class Instructions {
         BRNE = "1A";
         return BRNE;
     }
+
+    public String getAddressingMode(String mode) {
+        String addressingMode;
+        if (mode.equals("i")){
+            addressingMode = "0";
+            return addressingMode;
+        }
+        else {
+            addressingMode = "1";
+            return addressingMode;
+        }
+    }
+
+    public String getValue(Partitioner.ParsedData data, String value){
+        if (value.contains("0x")){
+            value = value.split("0x")[1];
+        }
+        else if  (!value.isEmpty()){
+            value = data.branches.get(value);
+        }
+        return value;
+    }
 }
+
+
